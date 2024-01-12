271be2a8c63c6c3dc3e638bec43b1c69bcc78054
zgjiangtoby <zgjiangtoby@gmail.com>
2024-01-12 14:48:03 +0800

    updated run

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-for shot in 2 4 6 8 16 32 64 100 128; do for seed in 1 2 3 4 5; do python adapter_fewshot.py --seed $seed --dataset_name 'politifact' --train_csv "./datasets/fakenewsnet/politifact_multi.csv" --img_path "./datasets/fakenewsnet/poli_img_all/" --shot $shot --save_path "./saved_adapter"; done; done
-
+ for shot in 2 8 16 32; do for seed in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20; do python adapter_fewshot.py --seed $seed --dataset_name 'politifact' --train_csv "./datasets/fakenewsnet/politifact_multi.csv" --img_path "./datasets/fakenewsnet/poli_img_all/" --shot $shot --save_path "./saved_adapter"; done; done
